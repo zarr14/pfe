cbcac5aa42517da0a7e271e9f6f8eeb7557fa6af
MOHAMED AMINE ZARRAK <118459605+zarr14@users.noreply.github.com>
2024-04-30 13:44:24 +0100

    first commit for project

---
+++ pfe/pfe-app/pfe-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './Navbar'
+function App() {
+
+
+
+  return (
+    <>
+    <Navbar/>
+
+     <h2>hello</h2>
+    </>
+  )
+}
+
+export default App
